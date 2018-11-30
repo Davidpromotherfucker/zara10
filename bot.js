650db500e53dfefafaf9d328ce43d92fdc591b16
Davidpromotherfucker <44382470+Davidpromotherfucker@users.noreply.github.com>
2018-11-30 14:40:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("10help",{type: 'LISTENING'})
+   client.user.setActivity("4help",{type: 'LISTENING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -37,7 +37,7 @@ const queue = new Map();
  
  
  
-var prefix = "10" 
+var prefix = "4" 
 client.on('message', async msg => {
     if (msg.author.bot) return undefined;
    
@@ -229,7 +229,7 @@ function play(guild, song) {
     serverQueue.textChannel.send(`بدء تشغيل : **${song.title}**`);
 }
  
-const adminprefix = "10";
+const adminprefix = "4";
 const devs = ['505811134528356352'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
@@ -274,6 +274,6 @@ ${prefix}queue ⇏ لمعرفة قآئمة التشغيل
    });
  
  
-  client.on('ready',async () => { client.channels.find(ch => ch.id === "517300550845333504" && ch.type === 'voice').join(); });
+  client.on('ready',async () => { client.channels.find(ch => ch.id === "517804477777182720" && ch.type === 'voice').join(); });
  
 client.login(process.env.BOT_TOKEN);
